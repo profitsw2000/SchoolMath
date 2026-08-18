2a0b87c9d75841dfbc2c9b509184c39ada24f4b8
profitsw2000 <profitsw2000@gmail.com>
2026-08-18 21:58:33 +0300

    Доделан файл MainApplicationGradlePlugin.kt для настройки в gradle файлах модулей-приложений.

--- build-logic/convention/src/main/kotlin/MainApplicationGradlePlugin.kt
+++ build-logic/convention/src/main/kotlin/MainApplicationGradlePlugin.kt
@@ -1,5 +1,7 @@
 import org.gradle.api.Plugin
 import com.android.build.api.dsl.ApplicationExtension
+import gradleplugins.configureKotlinAndroid
+import gradleplugins.libs
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 
@@ -12,7 +14,8 @@ class MainApplicationGradlePlugin: Plugin<Project> {
             }
 
             extensions.configure<ApplicationExtension> {
-
+                configureKotlinAndroid(this)
+                configureDefaultConfig(project)
             }
         }
     }
@@ -20,7 +23,10 @@ class MainApplicationGradlePlugin: Plugin<Project> {
     private fun ApplicationExtension.configureDefaultConfig(project: Project) {
         with(project) {
             defaultConfig {
-
+                applicationId = libs.findVersion("applicationId").get().requiredVersion
+                targetSdk = libs.findVersion("targetSdk").get().requiredVersion.toInt()
+                versionCode = libs.findVersion("versionCode").get().requiredVersion.toInt()
+                versionName = libs.findVersion("versionName").get().requiredVersion
             }
         }
     }
