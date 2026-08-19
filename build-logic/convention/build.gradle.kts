ba78afb2268a32a498d0f77742b3fcd215f50ff9
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-08-19 10:34:25 +0300

    В build.gradle.kts пакета build-logic/convention зарегистрированы кастомные плагины.

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -13,6 +13,13 @@ dependencies {
 
 gradlePlugin {
     plugins {
-
+        register("mainApplicationGradlePlugin") {
+            id = libs.plugins.sm.main.app.gradle.plugin.get().pluginId
+            implementationClass = "MainApplicationGradlePlugin"
+        }
+        register("mainLibraryGradlePlugin") {
+            id = libs.plugins.sm.main.lib.gradle.plugin.get().pluginId
+            implementationClass = "MainLibraryGradlePlugin"
+        }
     }
 }
