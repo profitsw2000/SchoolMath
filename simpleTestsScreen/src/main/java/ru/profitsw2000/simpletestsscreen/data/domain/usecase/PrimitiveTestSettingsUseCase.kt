cf86c7b7562a6fcdf23c916ac16c22879d5332fb
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-08-20 10:24:38 +0300

    В класс PrimitiveTestSettingsUseCase.kt добавлен метод для получения настроек теста.

--- simpleTestsScreen/src/main/java/ru/profitsw2000/simpletestsscreen/data/domain/usecase/PrimitiveTestSettingsUseCase.kt
+++ simpleTestsScreen/src/main/java/ru/profitsw2000/simpletestsscreen/data/domain/usecase/PrimitiveTestSettingsUseCase.kt
@@ -1,9 +1,19 @@
 package ru.profitsw2000.simpletestsscreen.data.domain.usecase
 
+import ru.profitsw2000.simpletestsscreen.data.domain.model.PrimitiveMathOperationType
+import ru.profitsw2000.simpletestsscreen.data.domain.model.PrimitiveTestSettingsModel
 import ru.profitsw2000.simpletestsscreen.data.domain.repository.PrimitiveTestSettingsRepository
 
 class PrimitiveTestSettingsUseCase(
     private val primitiveTestSettingsRepository: PrimitiveTestSettingsRepository
 ) {
 
+    fun getTestSettings(operationType: PrimitiveMathOperationType): PrimitiveTestSettingsModel {
+        return when(operationType) {
+            PrimitiveMathOperationType.ADDITION -> primitiveTestSettingsRepository.getAdditionTestSettings()
+            PrimitiveMathOperationType.SUBTRACTION -> primitiveTestSettingsRepository.getSubtractionTestSettings()
+            PrimitiveMathOperationType.MULTIPLICATION -> primitiveTestSettingsRepository.getMultiplicationTestSettings()
+            PrimitiveMathOperationType.DIVISION -> primitiveTestSettingsRepository.getDivisionTestSettings()
+        }
+    }
 }
