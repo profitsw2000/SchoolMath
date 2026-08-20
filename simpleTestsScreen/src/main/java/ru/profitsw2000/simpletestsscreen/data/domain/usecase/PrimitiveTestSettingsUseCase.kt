6245cca92100faf18951289103139c17c71e4d63
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-08-20 10:05:18 +0300

    Создан класс-перечисление для определения типа базовой математической операции.

--- simpleTestsScreen/src/main/java/ru/profitsw2000/simpletestsscreen/data/domain/usecase/PrimitiveTestSettingsUseCase.kt
+++ simpleTestsScreen/src/main/java/ru/profitsw2000/simpletestsscreen/data/domain/usecase/PrimitiveTestSettingsUseCase.kt
@@ -1,5 +1,9 @@
 package ru.profitsw2000.simpletestsscreen.data.domain.usecase
 
-class PrimitiveTestSettingsUseCase {
+import ru.profitsw2000.simpletestsscreen.data.domain.repository.PrimitiveTestSettingsRepository
+
+class PrimitiveTestSettingsUseCase(
+    private val primitiveTestSettingsRepository: PrimitiveTestSettingsRepository
+) {
 
 }
