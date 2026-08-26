154ba8d0f14a38b0d0e9fec78e172b21fc099320
profitsw2000 <profitsw2000@gmail.com>
2026-08-26 13:57:21 +0300

    Создан класс для хранения данных для задания примитивного математического теста.

---
+++ simpleTestsScreen/src/main/java/ru/profitsw2000/simpletestsscreen/data/domain/model/PrimitiveMathTaskModel.kt
@@ -0,0 +1,7 @@
+package ru.profitsw2000.simpletestsscreen.data.domain.model
+
+data class PrimitiveMathTaskModel(
+    val firstOperand: Int,
+    val secondOperand: Int,
+    val primitiveMathOperationType: PrimitiveMathOperationType
+)
---
+++ simpleTestsScreen/src/main/java/ru/profitsw2000/simpletestsscreen/data/domain/usecase/PrimitiveTestTaskGeneratorUseCase.kt
@@ -0,0 +1,6 @@
+package ru.profitsw2000.simpletestsscreen.data.domain.usecase
+
+class PrimitiveTestTaskGeneratorUseCase {
+
+    //suspend fun getAdditionTask(taskComplexityLevel: Int):
+}
