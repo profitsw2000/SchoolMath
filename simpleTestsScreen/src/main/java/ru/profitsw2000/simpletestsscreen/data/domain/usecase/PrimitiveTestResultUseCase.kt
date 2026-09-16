bdeb27634c5888562d750d2f07c89bcd0903cc61
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-09-16 10:55:52 +0300

    В классе для проверки результата вычисления, введённого пользователем, сделана функция проверки ответа для примера сложения.

--- simpleTestsScreen/src/main/java/ru/profitsw2000/simpletestsscreen/data/domain/usecase/PrimitiveTestResultUseCase.kt
+++ simpleTestsScreen/src/main/java/ru/profitsw2000/simpletestsscreen/data/domain/usecase/PrimitiveTestResultUseCase.kt
@@ -1,5 +1,6 @@
 package ru.profitsw2000.simpletestsscreen.data.domain.usecase
 
+import ru.profitsw2000.simpletestsscreen.data.domain.model.PrimitiveMathOperationType
 import ru.profitsw2000.simpletestsscreen.data.domain.model.PrimitiveMathTaskModel
 
 class PrimitiveTestResultUseCase {
@@ -8,7 +9,25 @@ class PrimitiveTestResultUseCase {
         primitiveMathTaskModel: PrimitiveMathTaskModel,
         calculationResult: Int
     ): Boolean {
-        return TODO()
+        return when(primitiveMathTaskModel.primitiveMathOperationType) {
+            PrimitiveMathOperationType.ADDITION ->
+                checkAdditionTestResult(
+                    primitiveMathTaskModel.firstOperand,
+                    primitiveMathTaskModel.secondOperand,
+                    calculationResult
+                )
+            PrimitiveMathOperationType.SUBTRACTION -> TODO()
+            PrimitiveMathOperationType.MULTIPLICATION -> TODO()
+            PrimitiveMathOperationType.DIVISION -> TODO()
+        }
+    }
+
+    private fun checkAdditionTestResult(
+        firstOperand: Int,
+        secondOperand: Int,
+        calculationResult: Int
+    ): Boolean {
+        return (firstOperand + secondOperand) == calculationResult
     }
 
 }
